d3634aba4c03eab4a294cbab664e09ffbd8a399b
Arina-S-dev <bonabovino@yahoo.com>
2022-11-16 13:36:23 +0100

    merge

--- data/1_seed.sql
+++ data/1_seed.sql
@@ -7,10 +7,10 @@ INSERT INTO "card" ("content", "color", "list_id")
 VALUES ('Carte 1', '#f0f', 1),
        ('Carte 2', '#0f0', 1);
 
-INSERT INTO "tag" ("name", "color")
+INSERT INTO "label" ("name", "color")
 VALUES ('Urgent', '#f00');
 
-INSERT INTO "card_has_tag" ("card_id", "tag_id")
+INSERT INTO "card_has_label" ("card_id", "label_id")
 VALUES (1, 1);
 
 COMMIT;
